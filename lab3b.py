ff2e7f2dd7358ebab3e23197f5e8e5e2b1cb80b1
Andres Wong <agwong-lam@myseneca.ca>
2024-05-31 13:54:47 -0400

    lab3b.py

---
+++ lab3b.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Lab 3 Part 1 script - functions (# are cooler)
+# Author ID: agwong-lam
+
+#simple addition
+def sum_numbers(number1, number2):
+    return number1 + number2
+
+#simple substraction
+def subtract_numbers(number1, number2):
+    return number1 - number2
+
+#simple multiplication
+def multiply_numbers(number1, number2):
+    return number1 * number2
+
+if __name__ == '__main__':
+    print(sum_numbers(10, 5))
+    print(subtract_numbers(10, 5))
+    print(multiply_numbers(10, 5))
+
